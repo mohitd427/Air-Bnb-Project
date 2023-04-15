b07329ecaf4c8e622498a00ab9ab8856c2a66e13
mohitd427 <mohitdon0013@gmail.com>
2023-04-16 03:07:00 +0530

    added Nextjs template and done navbar part

---
+++ livebnb-project/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/CientOnly'
+
+export const metadata = {
+  title: 'Live Bnb',
+  description: 'Air Bnb Clone',
+}
+
+const font = Nunito({
+  subsets: ['latin']
+})
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {/* <ClientOnly> */}
+          <Navbar />
+        {/* </ClientOnly> */}
+        {children}
+      </body>
+
+    </html>
+  )
+}
---
+++ livebnb-project/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-rose-500 text-xl'>Hello Live Bnb </div>
+    </>
+  )
+}
